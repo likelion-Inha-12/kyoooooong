33157e3e8ec893378e0041c729ebb4d6a801c3df
minkyeong <haileyre000b@naver.com>
2024-05-27 19:58:46 +0900

    swagger 세션 중간 제출

--- backseminar/myback/lionapp/views.py
+++ backseminar/myback/lionapp/views.py
@@ -10,6 +10,25 @@ from rest_framework.views import APIView,View
 from rest_framework.decorators import api_view
 from .serializers import PostSerializer
 
+from drf_yasg.utils import swagger_auto_schema
+
+from rest_framework.decorators import authentication_classes
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+@swagger_auto_schema(
+        method="POST", 
+        tags=["첫번째 view"],
+        operation_summary="post 생성", 
+        operation_description="post를 생성합니다.",
+        responses={
+            201: '201에 대한 설명', 
+            400: '400에 대한 설명',
+            500: '500에 대한 설명'
+        }
+)
+
+@authentication_classes([JWTAuthentication])
+@api_view(['POST'])
 def create_post(request):
     if request.method == 'POST':
         data = json.loads(request.body)
@@ -25,6 +44,8 @@ def create_post(request):
         return JsonResponse({'message':'success'})
     return JsonResponse({'message':'POST 요청만 허용됩니다.'})
 
+@authentication_classes([JWTAuthentication])
+@api_view(['GET'])
 def get_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
     data = {
@@ -35,6 +56,8 @@ def get_post(request, pk):
     }
     return JsonResponse(data, status=200)
 
+@authentication_classes([JWTAuthentication])
+@api_view(['DELETE'])
 def delete_post(request, pk):
     if request.method == 'DELETE':
         post = get_object_or_404(Post, pk=pk)
@@ -45,6 +68,8 @@ def delete_post(request, pk):
         return JsonResponse(data, status=200)
     return JsonResponse({'message':'DELETE 요청만 허용됩니다.'})
 
+@authentication_classes([JWTAuthentication])
+@api_view(['GET'])
 def get_comment(request, post_id):
     if request.method == 'GET':
         post = get_object_or_404(Post, pk=post_id)
@@ -60,6 +85,7 @@ def api_response(data, message, status):
     return Response(response, status=status)
 
 
+@authentication_classes([JWTAuthentication])
 @api_view(['POST'])
 def create_post_v2(request):
     post = Post(
@@ -73,7 +99,7 @@ def create_post_v2(request):
 
 
 class PostApiView(APIView):
-
+    authentication_classes = [JWTAuthentication]
     def get_object(self, pk):
         post = get_object_or_404(Post, pk=pk)
         return post
@@ -91,8 +117,9 @@ class PostApiView(APIView):
         
         message = f"id: {pk}번 포스트 삭제 성공"
         return api_response(message = message, status = status.HTTP_200_OK) 
-    
-    
+
+@authentication_classes([JWTAuthentication])    
+@api_view(['PATCH'])    
 def like(request, user_id, post_id):
     if request.method == 'PATCH':
         if UserPost.objects.filter(user_id=user_id, post_id=post_id).exists():
@@ -108,20 +135,24 @@ def like(request, user_id, post_id):
         userPost.save()
 
         return HttpResponse(status=204)
-
-       
+    
+@authentication_classes([JWTAuthentication])
+@api_view(['GET'])       
 def get_likes(request, post_id):
     if request.method == 'GET':
         post = get_object_or_404(Post, pk=post_id)
         like_count = UserPost.objects.filter(post_id=post_id).count()
         return JsonResponse({'message':f'{post_id}의 총 하트 수는 {like_count}입니다.'})
 
-
+@authentication_classes([JWTAuthentication])
+@api_view(['GET'])
 def sort_post(request):
     if request.method == 'GET':
        post_list = Post.objects.annotate(comment_count=Count('comments')).order_by('-comment_count') 
        return HttpResponse(post_list, status=200)
 
+@authentication_classes([JWTAuthentication])    
+@api_view(['POST'])
 def create_member(request):
     if request.method == 'POST':
         data = json.loads(request.body)
